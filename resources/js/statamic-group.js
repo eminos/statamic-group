fd629d55fddd9ef3b49f9c658f369597261a67f2
Casey Dwyer <casey@cubitcreative.com>
2023-10-11 08:20:25 -0500

    new index component + registration

--- resources/js/statamic-group.js
+++ resources/js/statamic-group.js
@@ -1,5 +1,7 @@
 import GroupFieldtype from './components/GroupFieldtype.vue';
+import GroupIndexFieldtype from './components/GroupIndexFieldtype.vue';
 
 Statamic.booting(() => {
     Statamic.$components.register('group-fieldtype', GroupFieldtype);
+    Statamic.$components.register('group-fieldtype-index', GroupIndexFieldtype);
 });
